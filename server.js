84a99676426802e7940dbc6d251048ab49254442
PEACE Developer <dev@peace.vn>
2026-09-09 22:57:58 +0700

    feat: Add prebuilt .next production bundle, server.js, and .htaccess proxy

---
+++ server.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = false;
+const hostname = '0.0.0.0';
+const port = parseInt(process.env.PORT, 10) || 3000;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> PEACE Attendance Server ready on port ${port}`);
+  });
+});
